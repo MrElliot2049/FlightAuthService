a5eed2d6de014fba3828f3fa3562ec5aa9946783
Adamya Chauhan <adamya1332004@gmail.com>
2026-04-22 17:20:57 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const {PORT} = require('./config/server-config')
+const app = express();
+
+const startServer = () => {
+
+        app.listen(PORT, (req,res) => {
+        console.log(`Server started listening on ${PORT}`);
+    });
+}
+
+startServer();
