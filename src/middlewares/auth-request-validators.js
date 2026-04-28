dbe5d17523a2f2c9e8757c780ca7da6d0dd8d0ea
Adamya Chauhan <adamya1332004@gmail.com>
2026-04-28 19:30:16 +0530

    Added auth middlewares

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,15 @@
+const validateUser = (req, res, next) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({
+            success : false,
+            data : {},
+            message : "Something went wrong.",
+            err : "Email or password is missing"
+        });
+    }
+    next();
+}
+
+module.exports = {
+    validateUser
+}
---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+    authReqValidator : require('./auth-request-validators')
+}
