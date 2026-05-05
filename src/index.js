5eb372cea3760d371483fbcea90a10a2534d9473
Adamya Chauhan <adamya1332004@gmail.com>
2026-05-05 14:40:38 +0530

    Authorisation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const {PORT} = require('./config/server-config')
 const app = express();
 const apiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
+const db = require('./models/index');
 
 const startServer = () => {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended:true }));
         app.use('/api',apiRoutes);
-        
+        // if (process.env.DB_SYNC) {
+        //     db.sequelize.sync({alter: true});
+        // }        
         app.listen(PORT, async (req,res) => {
         console.log(`Server started listening on ${PORT}`);
     });
